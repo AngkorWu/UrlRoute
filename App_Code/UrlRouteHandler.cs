537b18b1762f167465333aff8f63c9dc0f11fd6c
AndyZou <Andy.F.Zou@rrd.com>
2013-09-12 22:07:26 -0500

    Add url route files

---
+++ App_Code/UrlRouteHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Xml;
+
+/// <summary>
+/// Summary description for UrlRouteHandler
+/// </summary>
+public class UrlRouteHandler
+{
+	public UrlRouteHandler()
+    { }
+
+    public static void SetUrlRoute()
+    {
+        XmlDocument document = new XmlDocument();
+        string xmlPath = HttpContext.Current.Server.MapPath(ConfigurationManager.AppSettings["urlRouteMap"].ToString());
+        document.Load(xmlPath);
+        XmlNodeList routes = document.SelectNodes("Routes/Route");
+        foreach (XmlNode item in routes)
+        {
+            string routeName = item.Attributes["Name"].InnerText;
+            string virtualPath = item.SelectSingleNode("VirtualPath").InnerText + "/";
+            string physcialPath = item.SelectSingleNode("PhysicalPath").InnerText;
+
+            RouteValueDictionary defaultValues = new RouteValueDictionary();
+            XmlNodeList parameters = item.SelectNodes("Parameters/Parameter");
+            foreach (XmlNode parameter in parameters)
+            {
+                string parameterName = parameter.InnerText;
+                string defaultValue = parameter.Attributes["DefaultValue"].InnerText;
+                virtualPath += "{" + parameterName + "}/";
+                if (!String.IsNullOrEmpty(defaultValue))
+                {
+                    defaultValues.Add(parameterName, defaultValue);
+                }
+            }
+            virtualPath = virtualPath.Substring(0, virtualPath.Length - 1);
+
+            RouteTable.Routes.Add(routeName, new Route(virtualPath, defaultValues, new PageRouteHandler(physcialPath)));
+        }
+    }
+}
---
+++ Pages/Home/PersonHome.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Pages_Home_PersonHome : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("Person Name:" + RouteData.Values["userName"].ToString());
+        Response.Write("<br />");
+        Response.Write("Person Age:" + RouteData.Values["age"].ToString());
+    }
+}
